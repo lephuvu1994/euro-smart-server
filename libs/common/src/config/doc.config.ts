d93ecb04a4d42bbef32ba92d370fe74c0fa720f5
vulewenlian94@gmail.com <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-03-17 07:36:19 +0700

    fix: update docker file

--- libs/common/src/config/doc.config.ts
+++ libs/common/src/config/doc.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs(
   'doc',
   (): Record<string, any> => ({
-    name: `${process.env.APP_NAME} APIs Specification`,
+    name: `${process.env.APP_NAME?.toUpperCase()} APIs Specification`,
     description: 'Euro smarthome API documentation',
     version: '1.0',
     prefix: '/docs',

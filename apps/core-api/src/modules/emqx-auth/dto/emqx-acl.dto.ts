a0a2b0425d935f6040675aa30166750febb085d6
vulewenlian94@gmail.com <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-04-10 03:20:08 +0700

    fix(emqx): relax action validation constraints to allow uppercase EMQX webhooks formats

--- apps/core-api/src/modules/emqx-auth/dto/emqx-acl.dto.ts
+++ apps/core-api/src/modules/emqx-auth/dto/emqx-acl.dto.ts
@@ -18,11 +18,10 @@ export class EmqxAclDto {
   @ApiProperty({
     description: 'MQTT action',
     example: 'subscribe',
-    enum: ['publish', 'subscribe'],
+    enum: ['publish', 'subscribe', 'PUBLISH', 'SUBSCRIBE', 'PUBLISH(Q0)', 'SUBSCRIBE(Q0)', 'PUBLISH(Q1)', 'SUBSCRIBE(Q1)', 'PUBLISH(Q2)', 'SUBSCRIBE(Q2)'],
   })
   @IsString()
-  @IsIn(['publish', 'subscribe'])
-  action: 'publish' | 'subscribe';
+  action: string;
 
   @ApiProperty({
     description: 'MQTT client ID (sent by EMQX webhook)',

7da2d126dfcc0496543ee9952104181e5f867a95
vulewenlian94@gmail.com <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-04-10 04:01:27 +0700

    chore: point emqx webhooks to proxy 3002

--- test_auth1.js
+++ test_auth1.js
@@ -3,7 +3,7 @@ const { Client } = require('ssh2');
 const conn = new Client();
 conn.on('ready', () => {
   console.log('Client :: ready');
-  conn.exec('. ~/.bashrc && export PATH=$PATH:/var/lib/docker/rootfs/overlayfs/ebe47324590e724d76317b57eb89caf7660fec4a8a96340fdaee28b7f7c4a03b/usr/local/bin && sleep 5 && docker logs --tail 500 aurathink-core-api-prod | grep "RECEIVED_ACL:"', (err, stream) => {
+  conn.exec('. ~/.bashrc && export PATH=$PATH:/var/lib/docker/rootfs/overlayfs/ebe47324590e724d76317b57eb89caf7660fec4a8a96340fdaee28b7f7c4a03b/usr/local/bin && docker exec aurathink-emqx-prod sed -i \'s/3001/3002/g\' /opt/emqx/etc/emqx-custom.conf && docker restart aurathink-emqx-prod', (err, stream) => {
     if (err) throw err;
     stream.on('close', (code, signal) => {
       console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);

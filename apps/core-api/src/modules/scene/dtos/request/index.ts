b0a99cadbe3220e00a5e3d5eaef5b7701dcd0caa
vulewenlian94@gmail.com <hamai@MacBook-Air-cua-vulewenlian94gmailcom.local>
2026-04-12 00:38:34 +0700

    feat(scene): add runScene delay API

--- apps/core-api/src/modules/scene/dtos/request/index.ts
+++ apps/core-api/src/modules/scene/dtos/request/index.ts
@@ -3,3 +3,4 @@ export * from './update-scene.dto';
 export * from './scene-trigger.dto';
 export * from './location-report.dto';
 export * from './reorder-scenes.dto';
+export * from './run-scene.dto';
